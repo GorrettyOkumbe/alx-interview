4affe1794447cd8508421cc6c388b94b7750512c
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-12-25 23:23:47 +0300

    interview question

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,22 +9,21 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    else:
-        p_triangle = [0] * n
-        for i in range(n):
-            new_row = [0] * (i + 1)
-            new_row[0] = 1
-            new_row[len(new_row) - 1] = 1
+    p_triangle = [0] * n
+    for i in range(n):
+        new_row = [0] * (i + 1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
 
-            for j in range(1, i):
-                if j > 0 and j < len(new_row):
-                    a = p_triangle[i - 1][j]
-                    b = p_triangle[i - 1][j - 1]
-                    new_row[j] = a + b
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = p_triangle[i - 1][j]
+                b = p_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-            p_triangle[i] = new_row
+        p_triangle[i] = new_row
 
-        return p_triangle
+    return p_triangle
 
     
 
