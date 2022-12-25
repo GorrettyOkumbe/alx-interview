6fbdcc9eda2e805447840256e00c81317dd6eb2d
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-12-25 23:14:17 +0300

    interview question

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""pascal's triangle module"""
+
+def pascal_triangle(n):
+    """
+    n: Integer number
+    Renurns: 
+        list of lists representing pascal's triangle
+    """
+    if n <= 0:
+        return []
+    else:
+        p_triangle = [0] * n
+        for i in range(n):
+            new_row = [0] * (i + 1)
+            new_row[0] = 1
+            new_row[len(new_row) - 1] = 1
+
+            for j in range(1, i):
+                if j > 0 and j < len(new_row):
+                    a = p_triangle[i - 1][j]
+                    b = p_triangle[i - 1][j - 1]
+                    new_row[j] = a + b
+
+            p_triangle[i] = new_row
+
+        return p_triangle
+
+    
+
